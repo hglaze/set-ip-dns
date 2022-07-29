09c00322492c31878a48def7f69cf9abbce2ca9f
storh1118 <storh1118@outlook.com>
2022-07-29 23:15:53 +0800

    添加部分注释

--- src/main/main.ts
+++ src/main/main.ts
@@ -9,6 +9,8 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import path from 'path';
+// The app module, which controls your application's event lifecycle.应用的生命周期
+// The BrowserWindow module, which creates and manages application windows.新建和管理应用窗口
 import { app, BrowserWindow, shell, ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
@@ -69,6 +71,13 @@ const createWindow = async () => {
     return path.join(RESOURCES_PATH, ...paths);
   };
 
+  // A preload script runs before the renderer process is loaded,
+  // and has access to both renderer globals (e.g. window and document)
+  // and a Node.js environment.
+  // 在electron的进程设计中，主进程控制窗口和应用生命周期以及原生API，
+  // 同时，electron也给每个网页生成了渲染进程来渲染网页
+  // 两者之间要想传递信息或进行操作只能通过预加载脚本（preload）
+
   mainWindow = new BrowserWindow({
     show: false,
     width: 1024,
@@ -119,10 +128,16 @@ const createWindow = async () => {
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
+  // 注意OSX和其他系统的不同之处
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+
+// In Electron, browser windows can only be created after the app module's ready event is fired.
+// You can wait for this event by using the app.whenReady() API.
+// Call createWindow() after whenReady() resolves its Promise.
+// 网页窗口只能在模块加载完毕后创建
 
 app
   .whenReady()
@@ -131,6 +146,7 @@ app
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
+      // 注意OSX和其他系统的不同之处
       if (mainWindow === null) createWindow();
     });
   })
